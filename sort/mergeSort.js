b5e053851cc56648b61ae197c3fbe1245c29a309
LuckyFBB <976060700@qq.com>
2019-12-08 14:35:11 +0800

    更新归并排序

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -2,7 +2,7 @@
  * @Author: FBB
  * @Date: 2019-02-14 14:51:23
  * @LastEditors: FBB
- * @LastEditTime: 2019-12-07 17:12:11
+ * @LastEditTime: 2019-12-08 14:34:57
  * @Description: 归并排序
  */
 function mergeSort(arr) {
@@ -45,4 +45,38 @@ function __merge(arr, left, mid, right) {
       j++;
     }
   }
+}
+
+// 归并排序
+function mergeSort2(arr) {
+  const len = arr.length;
+  if (len < 2) {
+    return arr;
+  }
+  const mid = len / 2;
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+  return __merge2(mergeSort2(left), mergeSort2(right));
+}
+
+function __merge2(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    //两个数组都还有值的时候
+    if (left[0] > right[0]) {
+      //right的最小值更小
+      result.push(right.shift());
+    } else {
+      result.push(left.shift());
+    }
+  }
+  while (left.length) {
+    //right没有数据了
+    result.push(left.shift());
+  }
+  while (right.length) {
+    //left没有数据了
+    result.push(right.shift());
+  }
+  return result;
 }
